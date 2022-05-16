f647126b8f5b409d3dc7d79648a0b1562c933f87
Sayam Ajmal <itssanesayam@gmail.com>
2022-05-16 17:02:59 +0100

    css

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,17 +1,16 @@
 import logo from "../images/logo.png";
 import searchicon from "../images/searchicon.png";
 
-
 export const Navigation = () => {
-    return (
-        <div className="navigation">
-            <div className="container">
-                <img className="logo" src={logo} alt="instagram logo" />
-                <div className="search">
-                    <img className="searchIcon" src={searchicon} alt="search icon" />
-                    <span className="searchText">Search</span>
-                </div>
-            </div>
+  return (
+    <div className="navigation">
+      <div className="container">
+        <img className="logo" src={logo} alt="instagram logo" />
+        <div className="search">
+          <img className="searchIcon" src={searchicon} alt="search icon" />
+          <span className="searchText">Search</span>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
