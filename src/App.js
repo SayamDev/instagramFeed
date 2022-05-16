e3bfc6479ed7ab40e1504ed4ef10e41139ba389c
Sayam Ajmal <itssanesayam@gmail.com>
2022-05-16 15:21:49 +0100

    setup done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { Header } from "./components/headers";
+import { fetchImages } from "./utils";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [userInput, setUserInput] = useState();
+  const [title, setTitle] = useState();
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    fetchImages(setImages);
+  }, []);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setTitle(userInput);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header title={title} />
+      <form onSubmit={submitHandler}>
+        <input onChange={(e) => setUserInput(e.target.value)} />
+        <option value= "default" disabled hidden>
+
+        </option>
+      </form>
+      {title ? <h2>You wrote a title</h2> : <h2>Hurry up and write a title</h2>}
+      {title && <h2>Hooray</h2>}
+      {images.map((image, i) => {
+        return (
+          <img className="ig"
+            src={image.download_url}
+            alt={`random file from unsplash ${i}`}
+          />
+        );
+      })}
     </div>
   );
-}
+};
 
 export default App;
+
---
+++ src/utils/index.js
@@ -0,0 +1,9 @@
+export const fetchImages = async (setter) => {
+    try {
+        const response = await fetch("https://picsum.photos/v2/list");
+        const data = await response.json();
+       setter(data);
+    } catch (error) {
+        console.log(error)
+    }
+} 
